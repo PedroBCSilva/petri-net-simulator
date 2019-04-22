7582ff5f5a801f12ae404cbfafccd7c334442208
gcolling <colling.g2@gmail.com>
2019-04-22 09:28:21 -0300

    Versão funcional

--- cycle.py
+++ cycle.py
@@ -1,10 +1,11 @@
 from place import Place
 from transition import Transition
+from copy import deepcopy
 
 class Cycle:
     places = []
     transitions = []
 
     def __init__(self, places, transitions):
-        self.places = places
-        self.transitions = transitions
+        self.places = deepcopy(places)
+        self.transitions = deepcopy(transitions)
