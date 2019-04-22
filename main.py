bb700b5c7c849a37451f0547fa85274017169977
gcolling <colling.g2@gmail.com>
2019-04-21 23:55:16 -0300

    Falha no arc_in

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ class PetriNet:
     transitions = []
     arcs = []
     cycles = []
+    start = []
 
     def __init__(self, file_name):
         self.file_name = file_name
         self.init_data()
         # self.print_data()
-        # self.process()
+        self.process()
         output = Output(self.cycles, self.places, self.transitions)
         output.print()
 
@@ -29,10 +30,10 @@ class PetriNet:
                     is_start = False
                     if is_start_text == 'True':
                         is_start = True
-                    else: 
+                        self.start.append(Place(name, int(marks), is_start))
+                    else:
                         is_start = False
                     self.places.append(Place(name, int(marks), is_start))
-                    # TODO TIRAR O FALSE E COLOCAR SE É ENTRADA OU NÃO
                 elif "new_transition" in line:
                     (text, name) = line.split()
                     self.transitions.append(Transition(name))
@@ -48,8 +49,6 @@ class PetriNet:
                         transition.add_arc_out(arc)
 
                     self.arcs.append(arc)
-
-        self.cycles.append(Cycle(self.places, self.transitions))
 
     def find_place_by_name(self, name):
         for i in range(len(self.places)):
@@ -79,28 +78,14 @@ class PetriNet:
     #         print("place:", arc.place.name)
     #         print("transition:", arc.transition.name)
 
-    def process(self):
-        # while(not entrada empty || transition_enabled() == True)
-            while(transition_enabled() == True):
-                transition = get_next()
-                for arc in transition.arcs_in:
-                    tplace = arc.place
-                    subtract_marks(tplace, arc.cost)
-                for arc in transition.arcs_out:
-                    tplace = arc.place
-                    add_marks(tplace, arc.cost)
-                toggle_transition(transition)
-            evaluate()
-            self.cycles.append(Cycle(self.places, self.transitions))
-
     def get_next(self):
         for transition in self.transitions:
-            if transition.enabled() == True:
+            if transition.is_enabled() == True:
                 return transition
 
     def transition_enabled(self):
         for transition in self.transitions:
-            if transition.enabled() == True:
+            if transition.is_enabled() == True:
                 return True
         return False
 
@@ -118,7 +103,7 @@ class PetriNet:
 
     def toggle_transition(self, transition):
         for i in range(len(self.transitions)):
-            if tplace == self.transitions[i]:
+            if transition == self.transitions[i]:
                 self.transitions[i].toggle()
                 return
 
@@ -135,9 +120,33 @@ class PetriNet:
                         self.transitions[i].toggle()
                         return
 
+    def start_empty(self):
+        empty = True
+        for start in self.start:
+            if start.marks != 0:
+                empty = False
+        return empty
+
+    def process(self):
+        self.evaluate()
+        self.cycles.append(Cycle(self.places, self.transitions))
+        while(self.start_empty() == False or self.transition_enabled() == True):
+            
+            while(self.transition_enabled() == True):
+                transition = self.get_next()
+                for arc in transition.arcs_in:
+                    tplace = arc.place
+                    self.subtract_marks(tplace, arc.cost)
+                for arc in transition.arcs_out:
+                    tplace = arc.place
+                    self.add_marks(tplace, arc.cost)
+                self.toggle_transition(transition)
+            self.evaluate()
+            self.cycles.append(Cycle(self.places, self.transitions))
+
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("--file", dest="filename", help="Path of file", default="input.txt",
+    parser.add_option("--file", dest="filename", help="Path of file", default="input1.txt",
                      type="string")
 
     (options, args) = parser.parse_args()
